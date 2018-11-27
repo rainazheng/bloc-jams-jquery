f63bbf2d23b1a55900cbb4a8ab4417cd8b01910a
Raina <raina.zheng@gmail.com>
2018-11-27 13:05:01 -0500

    completed checkpoint 11 - time input

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -27,4 +27,19 @@
     const prevSong = album.songs[prevSongIndex];
     player.playPause(prevSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
 }
